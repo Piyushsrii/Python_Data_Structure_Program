3f9e3c86722504bf3a121c6d5a47bcf226684723
piyushsrii <piyushnamo888@gmail.com>
2020-09-26 21:16:33 +0530

    [Piyush] Refactor : Some small changes

--- Basic_python/Colr_listing.py
+++ Basic_python/Colr_listing.py
@@ -2,9 +2,8 @@
 are not present in color_list_2.
 Test Data :
 color_list_1 = set(["White", "Black", "Red"])
-color_list_2 = set(["Red", "Green"])
+color_list_2 = set(["Red", "Green"])ls
 Expected Output White , Black'''
-
 color_list_1 = set(["White", "Black", "Red"])
 color_list_2 = set(["Red", "Green"])
 print(color_list_1.difference(color_list_2))

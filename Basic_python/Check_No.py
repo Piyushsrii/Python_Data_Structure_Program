dd8fd36fd57deca2a9d536094024c615875b10f2
piyushsrii <piyushnamo888@gmail.com>
2020-09-26 18:03:01 +0530

    [Piyush] Added :Add here method to this program and find true or false

---
+++ Basic_python/Check_No.py
@@ -0,0 +1,16 @@
+'''rite a Python program to check whether a specified value is contained in a group of values.
+Test Data :
+3 -> [1, 5, 8, 3] : True
+-1 -> [1, 5, 8, 3] : False'''
+def is_group_member(group_data, n):
+    '''
+    :param self:
+    :param strVal:
+    :return:
+    '''
+    for value in group_data:
+        if n == value:
+            return True
+    return False
+print(is_group_member([1, 5, 8, 3], 3))
+print(is_group_member([5, 8, 3], -1))

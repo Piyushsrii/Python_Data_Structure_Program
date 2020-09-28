ef3caff808332449bf937c30f52ea162da09e0c3
piyushsrii <piyushnamo888@gmail.com>
2020-09-28 18:16:15 +0530

    [Piyush] Added : Create class and method and find the number how many time it occur

---
+++ Basic_core_Programing/Array/Occurence_Number.py
@@ -0,0 +1,22 @@
+'''Write a Python program to get the number of occurrences of a specified element in an
+array.'''
+
+
+class OccurenceInArray:
+    def countOccurence(self, arrOfNUm, value):
+        '''
+            :param:self
+            :param:arrOfNUm
+            :param:value
+            :return:
+            '''
+        print("Original array: " + str(arrOfNum))
+        print("Number of occurrences of the number ", value, " in array: " + str(arrOfNUm.count(value)))
+arr = OccurenceInArray()
+rangeVal = int(input("Enter The range of value you want to enter : "))
+arrOfNum = []
+for i in range(rangeVal):
+    val = int(input("Enter The value for array : "))
+    arrOfNum.append(val)
+Value = int(input("Enter The Value You Want To Find Ocuurence of : "))
+arr.countOccurence(arrOfNum, Value)

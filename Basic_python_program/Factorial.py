b24d5863df8f0235af1caaef6f9bcf9ad2c67f75
piyushsrii <piyushnamo888@gmail.com>
2020-09-30 00:36:37 +0530

    Adding : Calculate factorial of number

---
+++ Basic_python_program/Factorial.py
@@ -0,0 +1,14 @@
+from math import factorial
+class factor:
+    def factorial(n):
+        if n == 0:
+            return 1
+        elif n < 0:
+            return "number is in negative can't do factorial"
+        else:
+            return (n * (factorial(n - 1)))
+        print(factorial(5))
+        if __name__=='__main__':
+            calling=factor();
+            output=calling.factorial(5)
+            print(output)

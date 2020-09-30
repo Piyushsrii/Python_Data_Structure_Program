f823a887832417b2d3c689bd35799fc1bdb07ac4
piyushsrii <piyushnamo888@gmail.com>
2020-09-30 23:42:07 +0530

    Adding: Calculate the distance between two point

--- Function/Distance.py
+++ Function/Distance.py
@@ -1,3 +1,6 @@
+'''Write a program Distance.java that takes two integer command-line arguments x
+and y and prints the Euclidean distance from the point (x, y) to the origin (0, 0). The
+formulae to calculate distance = sqrt(x*x + y*y). Use Math.power function'''
 import math
 class Distance:
     def distance(x, y):

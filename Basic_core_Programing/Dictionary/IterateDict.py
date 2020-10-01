a65325be8758dd50fd8e700bf444c2ccb58603b5
piyushsrii <piyushnamo888@gmail.com>
2020-10-01 15:35:40 +0530

    Adding : Iterate the Dictionary

---
+++ Basic_core_Programing/Dictionary/IterateDict.py
@@ -0,0 +1,18 @@
+'''Write a Python program to iterate over dictionaries using for loops'''
+class IterateDictionary:
+
+    #create a method for iteration
+    def iterate(self, dictOfNum):
+        print("Dictionary : ", dictOfNum)
+        for key, value in dictOfNum.items():
+            print(key, " is the key belongs to " , dictOfNum[key])
+
+dictionary = IterateDictionary()
+rangeVal = int(input("Enter The range of value you want to enter : "))
+dictOfNum = { }
+for i in range(1,rangeVal+1):
+    key = input("Enter The Key : ")
+    val = input("Enter The value for dictionary : ")
+    dictOfNum.setdefault(key, val)
+
+dictionary.iterate(dictOfNum)

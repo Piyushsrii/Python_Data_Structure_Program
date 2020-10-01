ceacbc08990f61c991a2b8c61b6d74c4b9342575
piyushsrii <piyushnamo888@gmail.com>
2020-10-01 19:56:31 +0530

    Adding : iterate the element in set

---
+++ Basic_core_Programing/Set/SetIterate.py
@@ -0,0 +1,20 @@
+'''Write a Python program to iteration over sets.'''
+class IterateSets:
+
+    #create a method to iterate  the dict
+    def toIterate(self,dictOfNum):
+        numItrSet = set(dictOfNum)
+        for n in numItrSet:
+            print(n)
+
+rangeVal = int(input("Enter The range of value you want to enter : "))
+dictOfNum = []
+try:
+    for i in range(1, rangeVal + 1):
+        val = input("Enter The value : ")
+        dictOfNum.append(val)
+except ValueError:
+    print("enter the correct value")
+        if "__name__"=="__main__":
+            setData = IterateSets()
+            setData.toIterate(dictOfNum)
